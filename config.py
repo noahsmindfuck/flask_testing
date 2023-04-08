e3a24708c553b3cfa1e9f36627e7c9b193dc2dfc
noahsmindfuck <arche1@gmx.at>
2023-04-08 09:23:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    DB_PWD = os.environ.get('SECRET_KEY') or 'golf dowry cruelly gloater plural'
-    DB_HOST = os.environ.get('SECRET_KEY') or '37.252.189.147'
+    DB_PWD = os.environ.get('SECRET_KEY') or '***'
+    DB_HOST = os.environ.get('SECRET_KEY') or '***'
     DB_USER = os.environ.get('SECRET_KEY') or 'flask_testing'
     DB_NAME = os.environ.get('SECRET_KEY') or '/flask_testing'
     userpass = 'mysql+pymysql://' + DB_USER + ':' + DB_PWD + '@'
